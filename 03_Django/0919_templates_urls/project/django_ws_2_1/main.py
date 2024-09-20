2ff01e6732ccfcc618f5a1cdc9ad98ca94c29dfd
jeongin <cim6674@naver.com>
2024-09-20 15:31:04 +0900

    0920 add

--- 03_Django/0919_templates_urls/project/django_ws_2_1/main.py
+++ 03_Django/0919_templates_urls/project/django_ws_2_1/main.py
@@ -5,3 +5,13 @@ API_KEY = None
 params = {
     'ttbkey': API_KEY,
 }
+response = requests.get(API_URL).json()
+result = []
+for item in response['item']:
+    book = {
+        '저자': item['author'],
+        '제목' : item['title'],
+    }
+    result.append(book)
+
+print(result)
